54caf7060616a064f51e5af5e345bc5e427b1854
cuchi <cuchi@LAPTOP-B1JK6E79>
2025-04-14 09:12:14 +0200

    2

--- src/github/Hola.java
+++ src/github/Hola.java
@@ -1,16 +1,10 @@
 package github;
 
 public class Hola {
-
-
-		public static void main(String[] args) {
-			System.out.println("uno");
-			System.out.println("hola");
-			System.out.println("lunes");
-			System.out.println("entornos");
-
-
+	public static void main(String[] args) {
+		System.out.println("uno");
+		System.out.println("hola");
+		System.out.println("lunes");
+		System.out.println("entornos");
 	}
-
-
 }
