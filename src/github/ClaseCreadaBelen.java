059bd4e1b096b2a5d694359e3626563b84ba7d5e
BELEN <BELEN@DESKTOP-GV24251>
2025-05-10 20:43:13 +0200

    Modificado por Belén.

---
+++ src/github/ClaseCreadaBelen.java
@@ -0,0 +1,11 @@
+package github;
+
+public class ClaseCreadaBelen {
+
+	public static void main (String [] args) {
+		System.out.println("Clase modificada por belén");
+		
+		
+		
+	}//fin del main
+}//fin de la clase
